ce00285b3c347e347faa3b16b496ac312724e35e
Marcus Phi <marcus@marcusphi.com>
2022-04-13 23:12:07 -0500

    switch to function syntax

--- Movies.Domain/Services.fs
+++ Movies.Domain/Services.fs
@@ -5,14 +5,14 @@ open Movies.Domain.Models
 open Movies.Domain.Outbound
 
 type IMoviesService =
-  abstract member GetPopulars : Movie list
-  abstract member GetUpcoming : Movie list
+  abstract member GetPopulars : unit -> Movie list
+  abstract member GetUpcoming : unit -> Movie list
   abstract member GetById : id: string -> Movie
 
 type MoviesServiceImpl(moviesProvider: IMoviesProvider) =
 
   interface IMoviesService with
-    member this.GetPopulars =
+    member this.GetPopulars() =
       let movies = moviesProvider.GetPopulars
 
       if movies.IsEmpty then
@@ -20,7 +20,7 @@ type MoviesServiceImpl(moviesProvider: IMoviesProvider) =
       else
         movies
 
-    member this.GetUpcoming =
+    member this.GetUpcoming() =
       let movies = moviesProvider.GetUpcoming
 
       if movies.IsEmpty then
@@ -30,6 +30,7 @@ type MoviesServiceImpl(moviesProvider: IMoviesProvider) =
 
     member this.GetById(id: string) =
       let movie = moviesProvider.GetById id
+
       match movie with
       | Some m -> m
       | None -> raise (MoviesNotFound($"Movie with id {id} not found"))
